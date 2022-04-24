f4f7fad7f057e7411ecc01d5f303621a401e2dd8
Jay Jaeger <cube1us@gmail.com>
2022-04-24 15:40:13 +0000

    Start work on IBM 1415 Console Light Panel
    git-svn-id: svn://localhost/active/IBM1410Console/trunk@2921 8de05751-f04e-1843-ac3c-a4bcb19ff523

---
+++ IBM1410Console/UI1415L.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IBM1410Console
+{
+    public partial class UI1415L : Form
+    {
+        public UI1415L() {
+            InitializeComponent();
+        }
+    }
+}

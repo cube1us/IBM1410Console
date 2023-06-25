91ffed973f0c1339465848fd1eeee267dc1deaac
Jay Jaeger <cube1us@gmail.com>
2023-06-25 02:07:58 +0000

    ConsoleSettingsForm renamed to ComPortSettingsForm, added property save/restore
    git-svn-id: svn://localhost/active/IBM1410Console/trunk@3078 8de05751-f04e-1843-ac3c-a4bcb19ff523

---
+++ IBM1410Console/CoreSizeSettingsForm.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace IBM1410Console
+{
+    partial class CoreSizeSettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            coreSizeComboBox = new System.Windows.Forms.ComboBox();
+            SuspendLayout();
+            // 
+            // coreSizeComboBox
+            // 
+            coreSizeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            coreSizeComboBox.FormattingEnabled = true;
+            coreSizeComboBox.Items.AddRange(new object[] { "10000", "20000", "40000", "60000", "80000", "100000" });
+            coreSizeComboBox.Location = new System.Drawing.Point(59, 40);
+            coreSizeComboBox.MaxDropDownItems = 10;
+            coreSizeComboBox.Name = "coreSizeComboBox";
+            coreSizeComboBox.Size = new System.Drawing.Size(152, 23);
+            coreSizeComboBox.TabIndex = 0;
+            coreSizeComboBox.SelectedIndexChanged += coreSizeComboBox_SelectedIndexChanged;
+            // 
+            // CoreSizeSettingsForm
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(267, 107);
+            Controls.Add(coreSizeComboBox);
+            Name = "CoreSizeSettingsForm";
+            Text = "IBM 1410 FPGA Core Size";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox coreSizeComboBox;
+    }
+}
---
+++ IBM1410Console/CoreSizeSettingsForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IBM1410Console
+{
+    public partial class CoreSizeSettingsForm : Form
+    {
+
+        public CoreSizeSettingsForm() {
+            InitializeComponent();
+        }
+
+        public void setCoreSize(string size) {
+            coreSizeComboBox.SelectedItem = size;
+        }
+
+        public long getCoreSize() {
+            return (long) coreSizeComboBox.SelectedItem;
+        }
+
+        private void coreSizeComboBox_SelectedIndexChanged(object sender, EventArgs e) {
+            string s = coreSizeComboBox.SelectedItem.ToString();
+            Properties.Settings.Default.CoreSize = Int32.Parse(s);
+            Debug.WriteLine("FPGA core size set to " + Properties.Settings.Default.CoreSize);
+            Properties.Settings.Default.Save();
+        }
+    }
+}

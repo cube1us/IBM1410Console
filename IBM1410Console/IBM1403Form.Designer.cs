8fdbbe27a81ca272f3cfce8fbdb19e41dfbfc39e
Jay Jaeger <cube1us@gmail.com>
2026-03-22 22:39:06 +0000

    IBM1403Form - First commit of IBM1403 code - fixed a couple of bugs in the process.
    git-svn-id: svn://localhost/active/IBM1410Console/trunk@3330 8de05751-f04e-1843-ac3c-a4bcb19ff523

---
+++ IBM1410Console/IBM1403Form.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace IBM1410Console
+{
+    partial class IBM1403Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            printerStartButton = new System.Windows.Forms.Button();
+            printerCheckResetButton = new System.Windows.Forms.Button();
+            printerStopButton = new System.Windows.Forms.Button();
+            printerCarriageSpaceButton = new System.Windows.Forms.Button();
+            printerCarriageRestoreButton = new System.Windows.Forms.Button();
+            printerSingleCycleButton = new System.Windows.Forms.Button();
+            labelPrintReady = new System.Windows.Forms.Label();
+            labelPrintCheck = new System.Windows.Forms.Label();
+            labelEndOfForms = new System.Windows.Forms.Label();
+            labelFormsCheck = new System.Windows.Forms.Label();
+            labelSyncCheck = new System.Windows.Forms.Label();
+            carriageStopButton = new System.Windows.Forms.Button();
+            printerRichTextBox1 = new System.Windows.Forms.RichTextBox();
+            carriageTapeButton = new System.Windows.Forms.Button();
+            printerClearButton = new System.Windows.Forms.Button();
+            printerSaveButton = new System.Windows.Forms.Button();
+            SuspendLayout();
+            // 
+            // printerStartButton
+            // 
+            printerStartButton.BackColor = System.Drawing.Color.SeaGreen;
+            printerStartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            printerStartButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            printerStartButton.Location = new System.Drawing.Point(12, 12);
+            printerStartButton.Name = "printerStartButton";
+            printerStartButton.Size = new System.Drawing.Size(82, 53);
+            printerStartButton.TabIndex = 45;
+            printerStartButton.Text = "PRINT START";
+            printerStartButton.UseVisualStyleBackColor = false;
+            printerStartButton.Click += printerStartButton_Click;
+            // 
+            // printerCheckResetButton
+            // 
+            printerCheckResetButton.BackColor = System.Drawing.Color.LemonChiffon;
+            printerCheckResetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            printerCheckResetButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            printerCheckResetButton.Location = new System.Drawing.Point(100, 12);
+            printerCheckResetButton.Name = "printerCheckResetButton";
+            printerCheckResetButton.Size = new System.Drawing.Size(82, 53);
+            printerCheckResetButton.TabIndex = 49;
+            printerCheckResetButton.Text = "CHECK RESET";
+            printerCheckResetButton.UseVisualStyleBackColor = false;
+            // 
+            // printerStopButton
+            // 
+            printerStopButton.BackColor = System.Drawing.Color.Crimson;
+            printerStopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            printerStopButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            printerStopButton.Location = new System.Drawing.Point(188, 12);
+            printerStopButton.Name = "printerStopButton";
+            printerStopButton.Size = new System.Drawing.Size(82, 53);
+            printerStopButton.TabIndex = 50;
+            printerStopButton.Text = "PRINT STOP";
+            printerStopButton.UseVisualStyleBackColor = false;
+            printerStopButton.Click += printerStopButton_Click;
+            // 
+            // printerCarriageSpaceButton
+            // 
+            printerCarriageSpaceButton.BackColor = System.Drawing.Color.Ivory;
+            printerCarriageSpaceButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            printerCarriageSpaceButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            printerCarriageSpaceButton.Location = new System.Drawing.Point(12, 71);
+            printerCarriageSpaceButton.Name = "printerCarriageSpaceButton";
+            printerCarriageSpaceButton.Size = new System.Drawing.Size(82, 53);
+            printerCarriageSpaceButton.TabIndex = 51;
+            printerCarriageSpaceButton.Text = "CARRIAGE SPACE";
+            printerCarriageSpaceButton.UseVisualStyleBackColor = false;
+            // 
+            // printerCarriageRestoreButton
+            // 
+            printerCarriageRestoreButton.BackColor = System.Drawing.Color.Ivory;
+            printerCarriageRestoreButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            printerCarriageRestoreButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            printerCarriageRestoreButton.Location = new System.Drawing.Point(100, 71);
+            printerCarriageRestoreButton.Name = "printerCarriageRestoreButton";
+            printerCarriageRestoreButton.Size = new System.Drawing.Size(82, 53);
+            printerCarriageRestoreButton.TabIndex = 52;
+            printerCarriageRestoreButton.Text = "CARRIAGE RESTORE";
+            printerCarriageRestoreButton.UseVisualStyleBackColor = false;
+            // 
+            // printerSingleCycleButton
+            // 
+            printerSingleCycleButton.BackColor = System.Drawing.Color.Ivory;
+            printerSingleCycleButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            printerSingleCycleButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            printerSingleCycleButton.Location = new System.Drawing.Point(188, 71);
+            printerSingleCycleButton.Name = "printerSingleCycleButton";
+            printerSingleCycleButton.Size = new System.Drawing.Size(82, 53);
+            printerSingleCycleButton.TabIndex = 53;
+            printerSingleCycleButton.Text = "SINGLE CYCLE";
+            printerSingleCycleButton.UseVisualStyleBackColor = false;
+            // 
+            // labelPrintReady
+            // 
+            labelPrintReady.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            labelPrintReady.ForeColor = System.Drawing.Color.DimGray;
+            labelPrintReady.Location = new System.Drawing.Point(294, 20);
+            labelPrintReady.Name = "labelPrintReady";
+            labelPrintReady.Size = new System.Drawing.Size(57, 37);
+            labelPrintReady.TabIndex = 54;
+            labelPrintReady.Text = "PRINT READY";
+            labelPrintReady.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelPrintCheck
+            // 
+            labelPrintCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            labelPrintCheck.ForeColor = System.Drawing.Color.DimGray;
+            labelPrintCheck.Location = new System.Drawing.Point(370, 20);
+            labelPrintCheck.Name = "labelPrintCheck";
+            labelPrintCheck.Size = new System.Drawing.Size(57, 37);
+            labelPrintCheck.TabIndex = 55;
+            labelPrintCheck.Text = "PRINT CHECK";
+            labelPrintCheck.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelEndOfForms
+            // 
+            labelEndOfForms.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            labelEndOfForms.ForeColor = System.Drawing.Color.DimGray;
+            labelEndOfForms.Location = new System.Drawing.Point(294, 78);
+            labelEndOfForms.Name = "labelEndOfForms";
+            labelEndOfForms.Size = new System.Drawing.Size(67, 37);
+            labelEndOfForms.TabIndex = 56;
+            labelEndOfForms.Text = "END OF FORMS";
+            labelEndOfForms.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelFormsCheck
+            // 
+            labelFormsCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            labelFormsCheck.ForeColor = System.Drawing.Color.DimGray;
+            labelFormsCheck.Location = new System.Drawing.Point(294, 138);
+            labelFormsCheck.Name = "labelFormsCheck";
+            labelFormsCheck.Size = new System.Drawing.Size(57, 37);
+            labelFormsCheck.TabIndex = 57;
+            labelFormsCheck.Text = "PRINT READY";
+            labelFormsCheck.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelSyncCheck
+            // 
+            labelSyncCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            labelSyncCheck.ForeColor = System.Drawing.Color.DimGray;
+            labelSyncCheck.Location = new System.Drawing.Point(370, 138);
+            labelSyncCheck.Name = "labelSyncCheck";
+            labelSyncCheck.Size = new System.Drawing.Size(57, 37);
+            labelSyncCheck.TabIndex = 58;
+            labelSyncCheck.Text = "SYNC CHECK";
+            labelSyncCheck.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // carriageStopButton
+            // 
+            carriageStopButton.BackColor = System.Drawing.Color.Crimson;
+            carriageStopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            carriageStopButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            carriageStopButton.Location = new System.Drawing.Point(370, 225);
+            carriageStopButton.Name = "carriageStopButton";
+            carriageStopButton.Size = new System.Drawing.Size(83, 53);
+            carriageStopButton.TabIndex = 59;
+            carriageStopButton.Text = "CARRIAGE STOP";
+            carriageStopButton.UseVisualStyleBackColor = false;
+            carriageStopButton.Click += carriageStopButton_Click;
+            // 
+            // printerRichTextBox1
+            // 
+            printerRichTextBox1.Location = new System.Drawing.Point(472, 12);
+            printerRichTextBox1.Name = "printerRichTextBox1";
+            printerRichTextBox1.Size = new System.Drawing.Size(773, 426);
+            printerRichTextBox1.TabIndex = 60;
+            printerRichTextBox1.Text = "";
+            // 
+            // carriageTapeButton
+            // 
+            carriageTapeButton.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            carriageTapeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            carriageTapeButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            carriageTapeButton.Location = new System.Drawing.Point(12, 390);
+            carriageTapeButton.Name = "carriageTapeButton";
+            carriageTapeButton.Size = new System.Drawing.Size(123, 48);
+            carriageTapeButton.TabIndex = 61;
+            carriageTapeButton.Text = "Carriage Tape...";
+            carriageTapeButton.UseVisualStyleBackColor = false;
+            // 
+            // printerClearButton
+            // 
+            printerClearButton.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            printerClearButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            printerClearButton.Location = new System.Drawing.Point(141, 390);
+            printerClearButton.Name = "printerClearButton";
+            printerClearButton.Size = new System.Drawing.Size(110, 48);
+            printerClearButton.TabIndex = 62;
+            printerClearButton.Text = "Clear";
+            printerClearButton.UseVisualStyleBackColor = false;
+            // 
+            // printerSaveButton
+            // 
+            printerSaveButton.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            printerSaveButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            printerSaveButton.Location = new System.Drawing.Point(257, 390);
+            printerSaveButton.Name = "printerSaveButton";
+            printerSaveButton.Size = new System.Drawing.Size(110, 48);
+            printerSaveButton.TabIndex = 63;
+            printerSaveButton.Text = "Save...";
+            printerSaveButton.UseVisualStyleBackColor = false;
+            // 
+            // IBM1403Form
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(1257, 473);
+            Controls.Add(printerSaveButton);
+            Controls.Add(printerClearButton);
+            Controls.Add(carriageTapeButton);
+            Controls.Add(printerRichTextBox1);
+            Controls.Add(carriageStopButton);
+            Controls.Add(labelSyncCheck);
+            Controls.Add(labelFormsCheck);
+            Controls.Add(labelEndOfForms);
+            Controls.Add(labelPrintCheck);
+            Controls.Add(labelPrintReady);
+            Controls.Add(printerSingleCycleButton);
+            Controls.Add(printerCarriageRestoreButton);
+            Controls.Add(printerCarriageSpaceButton);
+            Controls.Add(printerStopButton);
+            Controls.Add(printerCheckResetButton);
+            Controls.Add(printerStartButton);
+            Name = "IBM1403Form";
+            Text = "IBM1403Form";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button printerStartButton;
+        private System.Windows.Forms.Button printerCheckResetButton;
+        private System.Windows.Forms.Button printerStopButton;
+        private System.Windows.Forms.Button printerCarriageSpaceButton;
+        private System.Windows.Forms.Button printerCarriageRestoreButton;
+        private System.Windows.Forms.Button printerSingleCycleButton;
+        private System.Windows.Forms.Label labelPrintReady;
+        private System.Windows.Forms.Label labelPrintCheck;
+        private System.Windows.Forms.Label labelEndOfForms;
+        private System.Windows.Forms.Label labelFormsCheck;
+        private System.Windows.Forms.Label labelSyncCheck;
+        private System.Windows.Forms.Button carriageStopButton;
+        private System.Windows.Forms.RichTextBox printerRichTextBox1;
+        private System.Windows.Forms.Button carriageTapeButton;
+        private System.Windows.Forms.Button printerClearButton;
+        private System.Windows.Forms.Button printerSaveButton;
+    }
+}
---
+++ IBM1410Console/IBM1403Form.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Net.Sockets;
+using System.Security.Policy;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IBM1410Console
+
+    /*
+    * Printer FPGA <-> PC Support Program protocols:
+    * 
+    * For PC => FPGA:
+    *    Channel Character [CR]:            0x85 (Both Channels)
+    *    Unit Record Device Char [UD]:      0x02 (Printer Ch1)
+    *    Status Operation Code Char [SO]:   0x01
+    *    Status from PC to FPGA:            [CR] [UD] [SO] [Status]
+    * 
+    * For FPGA => PC
+    *    Channel Character [CW]:            0x83 (Both channels)
+    *    Unit Record Device Char [UD]:      0x02 (Printer Ch1)
+    *    Unt Operation Char [OP]:           0x20 (print) or 0x2F (Carriage Control)
+    *    Message from FPGA to PC:           [CW] [UD] [OP] [data bytes - print line or CC char] [0x00]
+    * 
+    */
+
+{
+    public partial class IBM1403Form : Form
+    {
+
+        private SerialPort serialPort;
+        private SemaphoreSlim serialOutputSemaphore;
+        private SerialDataPublisher serialDataPublisher;
+        private UDPDataPublisher udpDataPublisher;
+        private UdpClient udpClient = null;
+        private SemaphoreSlim udpOutputSemaphore = null;
+        private byte[] printLine;
+
+        private Boolean printerReady = false;
+        private Boolean printerBusy = false;
+
+        public const int printerIsReady = 0x01;
+        public const int printerIsBusy = 0x02;
+
+        private int currentUnitRecordDevice = 0;
+        private int currentPrinterOperation = 0;
+        private int currentPrinterColumn = 0;
+        private byte currentCarriageControlCharacter = 0;
+
+        public const byte UNITRECORDFLAG = 0x85;
+        public const byte READERCH1FLAG = 0x01;
+        public const byte PRINTERCH1FLAG = 0x02;
+        public const byte PRINTERCH2FLAG = 0x42;
+
+        public const byte STATUSUPDATEOPERATION = 0x01;
+        public const byte PRINTEROPERATION = 0x20;
+        public const byte FORMSOPERATION = 0x2F;
+        public const int PRINTERLINELENGTH = 132;
+
+        private byte printerStatus = 0;               // Not Ready by default
+
+        public IBM1403Form(SerialDataPublisher serialDataPublisher,
+            UDPDataPublisher udpDataPublisher,
+            SerialPort serialPort, SemaphoreSlim serialOutputSemaphore,
+            UdpClient udpClient, SemaphoreSlim udpOutputSemaphore) {
+
+            this.serialPort = serialPort;
+            this.serialOutputSemaphore = serialOutputSemaphore;
+            this.udpDataPublisher = udpDataPublisher;
+            this.udpClient = udpClient;
+            this.udpOutputSemaphore = udpOutputSemaphore;
+            printLine = null;
+
+            InitializeComponent();      // Create the buttons, etc.
+            this.CreateHandle();        // Ensure controls are created before we get FPGA data
+
+            printerStartButton.Enabled = true;
+            printerStopButton.Enabled = true;
+
+            serialDataPublisher.UnitChannel1OutputEvent +=
+                new EventHandler<UnitRecordChannelEventArgs>(unitChannel1OperationAvailable);
+
+            udpDataPublisher.UDPUnitChannel1OutputEvent +=
+                new EventHandler<UDPUnitRecordChannelEventArgs>(unitChannel1UDPOperationAvailable);
+
+            Debug.WriteLine("Event Handlers for Serial and UDP Data Publishers (Printer) Registered.");
+
+        }
+
+        //  Right now, the unit record devices are just UDP, so ignore serial requests
+        void unitChannel1OperationAvailable(object sender, UnitRecordChannelEventArgs e) {
+            //  Right now, just using UDP, so ignore serial port requests.
+        }
+
+        //  Method that receives subscribed-to events for unit record messages.
+        //  (There are to such subscribers right now, the other being the reader/punch)
+        void unitChannel1UDPOperationAvailable(object sender, UDPUnitRecordChannelEventArgs e) {
+
+            //  The CPU has asked the 1414 to have a unit record device do something...
+            //  It might or might NOT be the reader...
+
+            //  If this really isn't for us, ignore it.
+
+            if (e.DispatchCode != UNITRECORDFLAG) {
+                return;
+            }
+
+            //  Loop through the bytes (hopefully / typically, the entire packet)
+
+            for (int i = 0; i < e.UDPLen; i++) {
+                //  TODO: Make sure the other unit record devices aren't active
+                if (currentUnitRecordDevice == 0) {
+                    //  If we are not currently processing a device, this byte is
+                    //  the device code.
+                    currentUnitRecordDevice = e.UDPBytes[i];
+                    currentPrinterOperation = 0;
+                }
+                else if (currentUnitRecordDevice == READERCH1FLAG) {
+                    //  The current unit record device is the reader, so ignore it.
+                    //  But this needs to be here because the reader message does NOT have
+                    //  a 0x00 terminator - so we need to reset the device code.
+                    currentPrinterOperation = 0;
+                    currentCarriageControlCharacter = 0;
+                    currentUnitRecordDevice = 0;
+                }
+                else if (currentUnitRecordDevice == PRINTERCH1FLAG) {
+                    //  The current unit record device is the printer, so process it.
+                    printerMessageInputAvailable((byte)e.UDPBytes[i]);
+                }
+                else if (e.UDPBytes[i] == 0) {
+                    //  For any other device (e.g., the punch, for now), just ignore the data until we see a 0x00
+                    currentUnitRecordDevice = 0;
+                }
+            }
+        }
+
+        //  Method to actually handle a byte of data from the FPGA (the IBM 1414, if you will)
+        //  that is destined for the printer.
+
+        void printerMessageInputAvailable(byte c) {
+
+            //  0 Terminates the printer request when receiving data, whether a print line or a carriage control request
+
+            if (c == 0) {
+                currentPrinterOperation = 0;
+                currentUnitRecordDevice = 0;
+                return;
+            }
+
+            //  The first byte should be an operation code.
+
+            if (currentPrinterOperation == 0) {
+                if ((c & PRINTEROPERATION) == PRINTEROPERATION) {
+                    currentPrinterOperation = c;
+                    if (currentPrinterOperation == PRINTEROPERATION) {
+                        printLine = new byte[PRINTERLINELENGTH];
+                        currentPrinterColumn = 0;
+                    }
+                    else if (currentPrinterOperation == FORMSOPERATION) {
+                        printLine = null;
+                    }
+                }
+                else {
+                    Debug.WriteLine("IBM1402Form.printerMessageInputAvailable: Invalid printer operation code: " + c.ToString("X2"));
+                    MessageBox.Show("IBM1402Form.printerMessageInputAvailable: Invalid printer operation code: " + c.ToString("X2"));
+                }
+                currentPrinterOperation = 0;
+                currentUnitRecordDevice = 0;
+            }
+
+
+            else {
+                if (c == 0) {
+                    if (currentPrinterOperation == PRINTEROPERATION) {
+                        if (currentPrinterColumn != PRINTERLINELENGTH) {
+                            Debug.WriteLine("IBM1403Form.printerMessageInputAvailable: Print line data image < 132 characters.");
+                            MessageBox.Show("IBM1403Form.printerMessageInputAvailable: Print line data image < 132 characters.");
+                        }
+                        printerRichTextBox1.AppendText(System.Text.Encoding.ASCII.GetString(printLine) + "\n");
+                        printLine = null;
+                        currentPrinterOperation = 0;
+                        currentUnitRecordDevice = 0;
+                    }
+                    else if (currentPrinterOperation == FORMSOPERATION) {
+                        if (currentCarriageControlCharacter == 0) {
+                            Debug.WriteLine("IBM1403Form.printerMessageInputAvailable: Empty carriage control message received.");
+                            MessageBox.Show("IBM1403Form.printerMessageInputAvailable: Empty carriage control message received.");
+                        }
+                        else {
+                            //  TODO:  Actually do the carriage control operation.
+                        }
+                    }
+                }
+                else if (currentPrinterOperation == FORMSOPERATION) {
+                    if (currentCarriageControlCharacter != 0) {
+                        Debug.WriteLine("IBM1403Form.printerMessageInputAvailable: Multiple byte carriage control message received.");
+                        MessageBox.Show("IBM1403Form.printerMessageInputAvailable: Multiple byte carriage control message received.");
+                    }
+                    else {
+                        currentCarriageControlCharacter = c;
+                    }
+                }
+                else if (currentPrinterOperation == PRINTEROPERATION && currentPrinterColumn < PRINTERLINELENGTH) {
+                    //  Strip the character's 0x40 parity bit, convert to ASCII, and append to the print line.
+                    printLine.Append((byte)IBM1410BCD.BCDtoASCII((byte)(c & 0x3f)));
+                    ++currentPrinterColumn;
+                }
+                else {
+                    Debug.WriteLine("IBM1403Form.printerMessageInputAvailable: Print line data image > 132 characters.");
+                    MessageBox.Show("IBM1403Form.printerMessageInputAvailable: Print line data image > 132 characters.");
+                }
+            }
+
+        }
+
+        //  Method to send status of the punch.  For now, just the stop and start
+        //  buttons.  In theory, it should also deal with a full stacker.
+
+        private void sendPrinterStatus() {
+            byte[] statusBuffer = new byte[4];
+
+            printerStatus = 0;
+            if (printerReady) {
+                printerStatus |= printerIsReady;
+            }
+            if (printerBusy) {
+                printerStatus |= printerIsBusy;
+            }
+
+            //  For now, serial port version NOT implemented.
+
+            //  Send the updated status to the FPGA...
+
+            udpOutputSemaphore.Wait();
+            statusBuffer[0] = UNITRECORDFLAG;
+            statusBuffer[1] = PRINTERCH1FLAG;
+            statusBuffer[2] = STATUSUPDATEOPERATION;
+            statusBuffer[3] = printerStatus;
+            udpClient.Send(statusBuffer, statusBuffer.Length);
+            udpOutputSemaphore.Release();
+
+        }
+
+        private void printerStartButton_Click(object sender, EventArgs e) {
+            printerReady = true;
+            printerBusy = false;
+            sendPrinterStatus();
+        }
+
+        private void printerStopButton_Click(object sender, EventArgs e) {
+            printerReady = false;
+            printerBusy = false;
+            sendPrinterStatus();
+        }
+
+        private void carriageStopButton_Click(object sender, EventArgs e) {
+            printerStopButton_Click(sender,e);
+        }
+    }
+}
